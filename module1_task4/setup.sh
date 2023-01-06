b52019269e63df66e9ea73e05fdea41a23b79abd
Hèdge <105491625+Hedgebzh@users.noreply.github.com>
2023-01-06 10:50:27 +0100

    Update setup.sh

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -2,4 +2,7 @@
 sudo apt-get update
 sudo apt install hugo
 sudo apt install make
+make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_0.84.0_Linux-64bit.deb
+apt install ./hugo_0.84.0_Linux-64bit.deb
 make build
